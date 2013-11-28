0edaaa6f570a345c48f1990c25c02a8c79de6a46
Patrick D. Kelly <e@pdk.to>
2013-11-27 21:10:27 -1000

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Bikexyz::Application.config.secret_token = '2c98b2a5f79f84043245e9728f1cc86faade28e09a748a8ffc981c2879a83836723ccd88f508aa1cae87b4733686d67ed8bd7fc0c5e2984ae2f70d63681bbea6'
