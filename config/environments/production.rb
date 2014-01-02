2dd2a65f728e6e74689ea52729974f383116e8d0
Patrick D. Kelly <e@pdk.to>
2014-01-02 00:13:00 -1000

    css cleanup

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -46,7 +46,7 @@ Bikexyz::Application.configure do
   # config.action_controller.asset_host = "http://assets.example.com"
 
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
-  config.assets.precompile += %w( search.js simple.css zepto.min.js )
+  config.assets.precompile += %w( search.js simple.css bootstrap.css bootstrap.min.css bikexyz_extras.css bootstrap.min.js bootstrap.js )
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
